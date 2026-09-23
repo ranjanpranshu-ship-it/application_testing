4c74b27cda43ecbf2482a95243f002a5bf6e0323
pranshu <ranjanpranshu029@gmail.com>
2026-09-23 13:16:30 +0530

    fixed the test file to return Docker Compose

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,8 +16,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Docker Compose!"', () => {
+      expect(appController.getHello()).toBe('Docker Compose!');
     });
   });
 });
